1d7a2ecc661b2b340a4d8c3b6cda897dfe3a1838
allwells <aleenfestus@gmail.com>
2021-01-20 05:34:50 +0100

    Finish contact form

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -19,49 +19,51 @@ const ContactContent = () => {
   return (
     <div>
       <div className="form-bg">
-        <div className="working-hours-container">
+        {/* <div className="working-hours-container">
           <div className="working-hours">
             <p></p>
           </div>
-        </div>
+        </div> */}
         <form className="contact-form" action="#contact-us-form" method="post">
           <div className="input-fields-container">
-            <div className="name-container">
-              <label htmlFor="fullname">Name</label>
-              <input
-                placeholder="eg. John Doe"
-                className="fullname"
-                name="fullname"
-                id="fullname"
-              />
+            <div className="input-fields">
+              <div className="name-container textfields">
+                <label htmlFor="fullname">Name</label>
+                <input
+                  placeholder="eg. John Doe"
+                  className="fullname inputs"
+                  name="fullname"
+                  id="fullname"
+                />
+              </div>
+              <div className="company-container textfields">
+                <label htmlFor="company">Company</label>
+                <input
+                  placeholder="eg. Harrylands Clinic"
+                  className="company inputs"
+                  name="company"
+                  id="company"
+                />
+              </div>
+              <div className="email-container textfields">
+                <label htmlFor="email">Email</label>
+                <input
+                  placeholder="eg. johndoe@gmail.com"
+                  className="email inputs"
+                  name="email"
+                  id="email"
+                />
+              </div>
             </div>
-            <div className="company-container">
-              <label htmlFor="company">Company</label>
-              <input
-                placeholder="eg. Harrylands Clinic"
-                className="company"
-                name="company"
-                id="company"
-              />
+            <div className="message-container textfields">
+              <label htmlFor="message">Message</label>
+              <textarea
+                placeholder="Leave a message..."
+                className="message"
+                name="message"
+                id="message"
+              ></textarea>
             </div>
-            <div className="email-container">
-              <label htmlFor="email">Email</label>
-              <input
-                placeholder="eg. johndoe@gmail.com"
-                className="email"
-                name="email"
-                id="email"
-              />
-            </div>
-          </div>
-          <div className="message-container">
-            <label htmlFor="message">Message</label>
-            <textarea
-              placeholder="Leave a message..."
-              className="message"
-              name="message"
-              id="message"
-            ></textarea>
           </div>
           <div className="submit-btn">
             <input className="submit" type="submit" value="Submit" />
